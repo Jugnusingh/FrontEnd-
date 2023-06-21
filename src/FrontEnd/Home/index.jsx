dfcaeca8584485f64b85909b6a7a762d8480ed1d
Jugnusingh <jugnu908@gmail.com>
2023-06-21 18:59:59 +0530

    add YouTube Video by API key on website

--- src/FrontEnd/Home/index.jsx
+++ src/FrontEnd/Home/index.jsx
@@ -1,32 +1,29 @@
 import React from 'react'
 import ImageSlider from '../Slider/imageSlider'
 import './Home.css'
-
-
 import YouTubeVideo from '../YouTube/YouTubeVideo';
-import GoogleReviews from '../Google/googleReview';
 const Home = ({ productData }) => {
   return (
     <div>
       <ImageSlider />
       <center><h1>About Us</h1></center>
       <div className='about-us-div'>
-        <div classname="about-us">
+        <div className="about-us">
           <h2>Who We Are </h2>
           <p>Tmply dummy text of the printing and typesetting indust Lorem Ipsum has been theindustry's standard dummy text ever since simply dummy text of the printing and etypesetting industry. Lorem Ipsum has been the induststandard dummy text ever since en an unknown printer took a galley of type scrambledmaining.</p>
         <h2>What We Do</h2>
         <p>Tmply dummy text of the printing and typesetting indust Lorem Ipsum has been theindustry's standard dummy text ever since simply dummy text of the printing and etypesetting industry. Lorem Ipsum has been the induststandard dummy text ever since en an unknown printer took a galley of type scrambledmaining.</p>
         </div>
-        <div classname="about-us">
+        <div className="about-us">
         <img className='about-image' src="Images/who.png" alt="PDF 1" />
         </div>
       </div>
       <br></br>
       <div className='about-us-div'>
-      <div classname="about-us">
+      <div className="about-us">
       <img className='about-image' src="Images/Homepage1.jpg" alt="PDF 1" />
       </div>
-      <div classname="about-us">
+      <div className="about-us">
         <h2>Who We Are </h2>
         <p>Tmply dummy text of the printing and typesetting indust Lorem Ipsum has been theindustry's standard dummy text ever since simply dummy text of the printing and etypesetting industry. Lorem Ipsum has been the induststandard dummy text ever since en an unknown printer took a galley of type scrambledmaining.</p>
       <h2>What We Do</h2>
@@ -34,7 +31,7 @@ const Home = ({ productData }) => {
       </div>
       </div>
       <br></br>
-      <center><h1>Our youtube Video</h1></center>
+      <center><h1>Our YouTube Video</h1></center>
       <div className='Youtube-cannel '>
       <YouTubeVideo/>
       </div>
@@ -46,7 +43,7 @@ const Home = ({ productData }) => {
       <br></br>
       <center><h1>write a Review </h1></center>
       <div className='Youtube-cannel'>
-       <GoogleReviews/>
+      
       </div>
     </div>
     
---
+++ src/FrontEnd/YouTube/YouTubeVideo.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './YouTubeVideo.css'
+const apiKey = 'apikey ';
+const channelId = 'channel_key';
+
+function YouTubeVideo() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+
+  const fetchVideos = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=10&order=date&type=video&key=${apiKey}`
+      );
+      setVideos(response.data.items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderVideos = () => {
+    const videoCount = videos.length;
+  
+    if (videoCount === 0) {
+      return <p>Loading videos...</p>;
+    }
+  
+    const duplicatedVideos = videos.concat(videos); // Duplicate the videos array
+  
+    return duplicatedVideos.map((video, index) => (
+      <div
+        key={video.id.videoId + '_duplicate_' + index}
+        className="video-card"
+        onClick={() =>
+          window.open(`https://www.youtube.com/watch?v=${video.id.videoId}`, '_blank')
+        }
+      >
+        <div className="video-thumbnail">
+          <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+        </div>
+        <div className="video-details">
+          <h3>{video.snippet.title}</h3>
+        </div>
+      </div>
+    ));
+  };
+  return (
+    <div className="video-slider">
+      {videos.length > 0 ? (
+        <div className="slider-content">{renderVideos()}</div>
+      ) : (
+        <p>Loading videos...</p>
+      )}
+    </div>
+  );
+}
+
+export default YouTubeVideo;
